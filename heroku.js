3cf7089d3fa90454031cc6cf55d8cec43663fcf7
AnhtuanUit <anhtuan7194@gmail.com>
2016-08-15 22:03:16 +0700

    v1

---
+++ heroku.js
@@ -0,0 +1,72 @@
+
+exec = require('child_process');
+
+//socket client.js
+var io = require('socket.io-client');
+var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NzllOWJhMzcxNzgxMWUwMjA3NTI1OGIiLCJ1c2VybmFtZSI6InR1YW4iLCJhdmF0YXIiOiJmZW1hbGUucG5nIiwiaWF0IjoxNDcwMDEzMDU0fQ.wb5Vv6pJc9HVF_YKkZLYHi0zT3EebAMIQz0apobDQq0';
+var socket = io.connect('https://enigmatic-temple-98253.herokuapp.com?token=' + token, {reconnect: true});
+
+
+//define the routes from the external file
+function sendMessage(socket, type, nodeChanel){
+	var message = type*100 + 1*10 + nodeChanel;
+	exec.execFile('./remote',
+		[message],
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			if( stdout.indexOf("Got this response") > -1 ){
+				type = type == 1 ? 0 : 1;
+
+				var state = stdout.split('Got this response ')[1].split('.')[0];
+				socket.emit('updateNode', state);
+           			if(i!=0 && activityType == 1){
+					var message_2 = activityType*100 + 10 + vanSub;
+					socket.emit('updateNode', message_2);
+				}
+				
+			}
+			if (error !== null) {
+				console.log('exec error: ' + error);
+				socket.emit('updateNode', 'error');
+			}
+		});
+}
+
+var activityType;
+
+var pumpChanel;
+var vanArray;
+var vanSub;
+
+var i, howManyTimes, time;
+var newControl = 0;
+socket.on('chat', function (data) {
+	pumpChanel = data.pump[0].chanel;
+	activityType = data.activityType;
+	vanArray = data.van;
+	howManyTimes = vanArray.length;
+	newControl == 1;
+	i = 0;
+	console.log(data);
+	f(); 
+});
+
+
+function f() {
+	newControl = 0;
+	sendMessage(socket, activityType, vanArray[i].chanel);
+	time = vanArray[i].estimatedTime;
+	if(activityType == 4)
+		time = 5;
+	if(i!=0){
+		vanSub = vanArray[i-1].chanel;
+	}
+	i++;
+	if( i < howManyTimes && newControl == 0){
+		setTimeout( f, time * 1000 );
+	}
+}
+
+
+//just some output to know everything is working
+console.log("Run client!!!");
--- remote.cpp
+++ remote.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <iostream>
 #include <RF24/RF24.h>
+#include <String.h>
 
 using namespace std;
 //RF24 radio("/dev/spidev0.0",8000000 , 25);  
@@ -15,16 +16,16 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 //const uint8_t pipes[][6] = {"1Node","2Node"};
 
 // hack to avoid SEG FAULT, issue #46 on RF24 github https://github.com/TMRh20/RF24.git
-int message[6];
-int got_message[6];
-
+char message[35];
+int got_message;
+char charMessage[35] = "800100-06040005-0166026603660666"; 
+char charMessage1[35] = "800100-06040005-0066046605660666"; 
 void setup(void){
 	//Prepare the radio module
 	printf("\nPreparing interface\n");
 	radio.begin();
 	radio.setRetries( 15, 15);
 	//	radio.setChannel(0x4c);
-	//	radio.setPALevel(RF24_PA_MAX);
 	//	radio.setPALevel(RF24_PA_MAX);
 
 	radio.printDetails();
@@ -34,12 +35,10 @@ void setup(void){
 
 }
 
-bool sendMessage(){
-	//This function send a message, the 'action', to the arduino and wait for answer
-	//Returns true if ACK package is received
-	//Stop listening
+bool sendMessage(char message[]){
+
 	radio.stopListening();
-	printf("Now sending  %d...", message[2]);
+	printf("Now sending  %d...", message);
 
 	//Send the message
 	bool ok = radio.write( &message, sizeof(message) );
@@ -67,33 +66,42 @@ bool sendMessage(){
 	}else{
 		//If we received the message in time, let's read it and print it
 		radio.read( &got_message, sizeof(got_message) );
-		printf("Yay! Got this response %d.\n\r",got_message[2]);
+		printf("Yay! Got this response %d.\n\r",got_message);
 		return true;
 	}
 }  
 
 int main( int argc, char ** argv){
-	message[0] = atoi(argv[1]);
-	message[1] = atoi(argv[2]);
-	message[2] = atoi(argv[3]);
+	message = atoi(argv[1]);
+
 	setup();
 	bool switched = false;
 	int counter = 0;
 
 	//Define the options
 
-	while(counter < 5){
-
 		printf("\n Talking with my NRF24l01+ friends out there....\n");
 
 		while(switched == false && counter < 5){
 
-			switched = sendMessage();
+			switched = sendMessage(charMessage);
 			
 			counter ++;
 
-			delay(1000);
+			delay(3000);
 		}
-		
-	}
+		if (counter < 5) {
+		return 0;
+		} else {
+		return 2;
+		}
+	
+}
+
+void checkSum(){
+	char buff[5] = "this is a test string";
+	char subbuff[5];
+	memcpy( subbuff, &buff[10], 4 );
+	subbuff[4] = '\0';
+	printf(subbuff);
 }
